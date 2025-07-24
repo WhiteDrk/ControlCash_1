8ac214e2a4aa64a2c22fff0765eb3c874dbccd74
alonso <daniorto621@gmail.com>
2025-07-24 08:06:40 -0600

    Version 1 al 24/07/2025 vv

--- src/BaseDatos/Conexion.py
+++ src/BaseDatos/Conexion.py
@@ -1,9 +1,41 @@
-import mysql.connector
+import pymysql
 
-def conectar_bd():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="tu_contraseña",
-        database="control_cash"
-    )
+class BDConexion:
+    def __init__(self):
+        self.__host = 'localhost'
+        self.__puerto = 3306
+        self.__usuario = 'root'  # Cambiar según tú usuario
+        self.__contrasena = 'tu_contraseña'  # Contraseña
+        self.__bd = 'control_cash'  # Nombre de tu base de datos
+        self.__conexion = None
+
+    def get_conexion(self):
+        #Devuelve la conexión actual
+        return self.__conexion
+
+    def conectar_db(self):
+        #Establece una conexión a la base de datos
+        try:
+            self.__conexion = pymysql.connect(
+                host=self.__host,
+                port=self.__puerto,
+                user=self.__usuario,
+                password=self.__contrasena,
+                database=self.__bd
+            )
+            print("✅ Conexión a la base de datos exitosa.")
+        except pymysql.MySQLError as error:
+            print(f"❌ Error al conectar a la base de datos: {error}")
+        return self.__conexion
+
+    def cerrar_conexion(self):
+        #Cierra la conexión a la base de datos
+        if self.__conexion:
+            try:
+                self.__conexion.close()
+                print("🔒 Conexión cerrada correctamente.")
+            except pymysql.MySQLError as error:
+                print(f"❌ Error al cerrar la conexión: {error}")
+        return self.__conexion
+
+#Importante cerrar la base de datos después de cada uno de los usos
